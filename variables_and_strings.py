6b8372e0bf32f98eccdeeddd5bd0e8b1dfef1d6e
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-07-06 17:31:57 -0500

    finished with variable naming rules and conventions

--- variables_and_strings.py
+++ variables_and_strings.py
@@ -10,3 +10,19 @@ print(num_of_cats_crazy_cat_lady)
 
 print("Amount of cats I have:")
 print(num_of_cats_normal * 0)
+
+_valid_variable_name = "underscores and letters to start"
+valid_variable_name2 = "numbers are cool, but not for the start of the variable name"
+
+print(_valid_variable_name)
+print(valid_variable_name2)
+
+different_variable = True
+DIFFERENT_VARIABLE = False
+
+print(different_variable == DIFFERENT_VARIABLE)
+
+print("Most variables should be snake cased and lowercase")
+print("Constants are uppercase")
+print("Camel case is used for classes")
+print("Double underscore are supposed to be private or left alone")
