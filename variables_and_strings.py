b4232c2da63959b93942a2c9977c798741e695c3
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-07-06 16:07:26 -0500

    variables and strings

---
+++ variables_and_strings.py
@@ -0,0 +1,12 @@
+num_of_cats_normal = 2
+
+num_of_cats_crazy_cat_lady = num_of_cats_normal * 1000
+
+print("Normal amount of cats:")
+print(num_of_cats_normal)
+
+print("Crazy cat lady amount of cats:")
+print(num_of_cats_crazy_cat_lady)
+
+print("Amount of cats I have:")
+print(num_of_cats_normal * 0)
