412ace101e887aec4b055ea5be3161263b1a37c5
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-07-07 19:15:54 -0500

    added conditionals.py

---
+++ 7-conditionals.py
@@ -0,0 +1,10 @@
+number = 1
+
+if(number > 50):
+    print("Big Numberrr!!")
+elif(number > 25):
+    print("Medium Numberrr!!")
+else:
+    print("Small Numberrrr!!!")
+
+print("Indentation matters.. instead of curly braces in JS, but in Python an indentation is used to specify the body (along with the colon after the conditional)")
